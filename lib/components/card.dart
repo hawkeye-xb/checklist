71fce2cd8d3e48d868c2762d8234583326e4d842
lizhaowen <lizhaowen0722@gmail.com>
2024-03-08 20:58:01 +0800

    ui

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -44,27 +44,26 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0), // bottom 有more按钮提供了8.0的padding
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 10.0,
-            offset: Offset(0.0, 0.0),
-          ),
-        ],
-      ),
-      child: Flex(
-        direction: Axis.vertical,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: Text(
+    return Stack(children: [
+      Container(
+        padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0), // bottom 有more按钮提供了8.0的padding
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10.0,
+              offset: Offset(0.0, 0.0),
+            ),
+          ],
+        ),
+        child: Flex(
+          direction: Axis.vertical,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
               title,
               style: const TextStyle(
                 fontSize: 20.0,
@@ -73,13 +72,7 @@ class CardWidget extends StatelessWidget {
                 overflow: TextOverflow.ellipsis,
               ),
             ),
-          ),
-          // 占位
-          // const SizedBox(height: 8.0,),
-          // 状态展示
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: Flex(
+            Flex(
               direction: Axis.horizontal,
               children: [
                 // items每个元素的checked属性都为true时，返回true。
@@ -88,7 +81,7 @@ class CardWidget extends StatelessWidget {
                 if (!items.every((element) => element.checked)) Text(
                   '${items.where((element) => element.checked).length}/${items.length}',
                   style: const TextStyle(
-                    fontSize: 14.0,
+                    fontSize: 12.0,
                     height: 1.5,
                     color: Colors.black38,
                   ),
@@ -97,85 +90,86 @@ class CardWidget extends StatelessWidget {
                 if (favorite) const Icon(Icons.star, size: 14.0,),
               ],
             ),
-          ),
-          Flexible(
-            flex: 1,
-            child: ListView.builder(
-              // itemCount: items.length,
-              itemCount: 
-                items.length > 3
-                  ? 3
-                  : items.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Flex(
-                  direction: Axis.horizontal,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Transform.scale(
-                      scale: 0.8,
-                      child: AbsorbPointer(
-                        absorbing: true,
-                        child: SizedBox(
-                          width: 24.0,
-                          height: 24.0,
-                          child: Radio(
-                            groupValue: true,
-                            value: items[index].checked,
-                            onChanged: (bool? value) {},
+            Flexible(
+              flex: 1,
+              child: ListView.builder(
+                // itemCount: items.length,
+                itemCount: 
+                  items.length > 3
+                    ? 3
+                    : items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Flex(
+                    direction: Axis.horizontal,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Transform.scale(
+                        scale: 0.8,
+                        child: AbsorbPointer(
+                          absorbing: true,
+                          child: SizedBox(
+                            width: 24.0,
+                            height: 24.0,
+                            child: Radio(
+                              groupValue: true,
+                              value: items[index].checked,
+                              onChanged: (bool? value) {},
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: RichText(
-                        text: TextSpan(
-                          text: items[index].content,
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            height: 1.5,
-                          ).merge(
-                            items[index].checked 
-                              ? const TextStyle(decoration: TextDecoration.lineThrough, color: Colors.black38,)
-                              : const TextStyle(color: Colors.black87),
+                      Flexible(
+                        flex: 1,
+                        child: RichText(
+                          text: TextSpan(
+                            text: items[index].content,
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              height: 1.5,
+                            ).merge(
+                              items[index].checked 
+                                ? const TextStyle(decoration: TextDecoration.lineThrough, color: Colors.black38,)
+                                : const TextStyle(color: Colors.black87),
+                            ),
                           ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
                         ),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                      ),
-                    )
-                    ,
-                  ],
-                );
-              },
+                      )
+                      ,
+                    ],
+                  );
+                },
+              ),
             ),
-          ),
-          const SizedBox(height: 8.0,),
-          Flex(
-            direction: Axis.horizontal,
-            crossAxisAlignment: CrossAxisAlignment.center, // 子元素居中对齐
-            children: [
-              Text(
-                _handleTimestamp(timestamp),
-                // '2024/11/12',
-                style: const TextStyle(
-                  fontSize: 14.0,
-                  height: 1.5,
-                  color: Colors.black38
-                ),
-              ),
-              Flexible(
-                flex: 1,
-                child: Container(), // Flexible用于填充空间
-              ),
-              const SizedBox(width: 4.0,),
-              ...extraChildren,
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
-    );
+      Positioned(
+        left: 0.0,
+        right: 0.0,
+        bottom: 0.0,
+        child: Flex(
+          direction: Axis.horizontal,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(width: 12.0,),
+            Text(
+              _handleTimestamp(timestamp),
+              style: const TextStyle(
+                fontSize: 14.0,
+                height: 1.5,
+                color: Colors.black38
+              ),
+            ),
+            Flexible(child: Container(),),
+            ...extraChildren,
+          ],
+        ),
+      ),
+    ],);
+    
   }
 }
 
